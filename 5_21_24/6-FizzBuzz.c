760035e1451d83def0aaeca9e8c5e9f4d0646275
root <dastelljes>
2024-05-21 19:29:22 +0000

    fizzbuzz

--- 5_21_24/6-FizzBuzz.c
+++ 5_21_24/6-FizzBuzz.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+void fizzBuzz(int n)
+{
+    int i;
+
+    for(i = 1; i <= n; i++)
+    {
+        if ((i % 3) && (i % 5))
+        {
+            printf("%d", i);
+        }
+        if (!(i % 3))
+        {
+            printf("Fizz");
+        }
+        if (!(i % 5))
+        {
+            printf("Buzz");
+        }
+        printf("\n");
+    }
+}
+
 int main(void)
 {
-
+        fizzBuzz(50);
+        return (0);
 }
