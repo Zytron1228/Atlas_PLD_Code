a541323d33ce61419fe55055b2ee7afa4186c324
Daniel Stelljes <dastelljes@outlook.com>
2024-06-04 15:09:14 -0500

    insignificant changes

--- 6_04_24/1-sum_arr.c
+++ 6_04_24/1-sum_arr.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 int main(void)
 {
-    int arr[4] = {1, 2, 3, 5};
+    int arr[5] = {1, 2, 3, 5, 6};
 
-    printf("sum: %d\n", sum(arr, 4));
-    return 0;
+    printf("sum: %d\n", sum(arr, 5));
+
+    return (0);
 }
 
 int sum(int *array, int size)
@@ -13,7 +14,7 @@ int sum(int *array, int size)
     if (size == 1)
         return (array[0]);
     array[size - 2] += array[size - 1];
-    printf ("%d\n", array[size - 1]);
-    return sum(array, size - 1);
+    printf("%d\n", array[size - 1]);
 
+    return sum(array, size - 1);
 }
