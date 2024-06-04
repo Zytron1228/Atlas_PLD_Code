a4f3f9b31903d5cf22da3e502d1b3b6bbbd8a052
Daniel Stelljes <dastelljes@outlook.com>
2024-06-04 12:46:34 -0500

    removed the print statements to make code look much simpler

--- 6_04_24/0-rev_array.c
+++ 6_04_24/0-rev_array.c
@@ -11,44 +11,15 @@ void reverseArray(int *array, int size)
     int i;
     int reversed[size];
 
-    printf("size: %d\n", size);
-
-        printf("reversed: ");
-        for (int k = 0; k < 4; k++)
-        {
-            printf("%d, ", reversed[k]);
-        }
-        putchar('\n');
-
     for (i = 0; i <= size; i++)
     {
         reversed[i] = array[i];
-        printf("inside for loop 1\n");
-        printf("size: %d\n", size);
-        printf("reversed: ");
-        for (int k = 0; k < 4; k++)
-        {
-            printf("%d, ", reversed[k]);
-        }
-        putchar('\n');
     }
+
     int s = size;
-
-    printf("before for loop 2\n");
-    printf("size: %d\n", size);
-
-    for (int n = 0; n < size; n++)
+    for (i = 0; i < size; i++)
     {
-        printf("inside for loop 2\n");
-        printf("size: %d\n", size);
-        printf("s: %d\n", s);
-        array[n] = reversed[s - 1];
         s--;
-        printf("array: ");
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%d, ", array[j]);
-        }
-        putchar('\n');
+        array[i] = reversed[s];
     }
 }
