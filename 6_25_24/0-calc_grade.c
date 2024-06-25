f4f618d3aeb6a3b885de04389077692b3e21db45
Daniel Stelljes <dastelljes@outlook.com>
2024-06-25 12:00:32 -0500

    start PLD code for first half of PLD 6-25-24 for grade calculator

---
+++ 6_25_24/0-calc_grade.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * calc_grade - calculates a grade percentage
+ *
+ * Return: a grade as a percentage/float
+ */
+float calc_grade()
+{
+	int max_categories;
+	float max_grade;
+	float total_grade;
+	int grade_weight;
+
+	printf("Enter grade category: ");
+	scanf("%i", max_categories);
+	printf("Enter grade weight: ");
+	scanf("%i", grade_weight);
+	printf("Enter current grades: ");
+	scanf("");
+
+	while (max_grade <= 100)
+	{
+
+	}
+
+
+
+
+
+	return (0);
+}
---
+++ 6_25_24/0-instructions.md
@@ -0,0 +1,15 @@
+# 0-calc_grade
+
+#### Write a program that calculates your grade.
+
+- A user should define:
+  - Grade Categories
+  - Grade Weights
+
+
+- Outputs:
+  - It should output current grade
+  - Potential grade for remaining project grades
+
+
+- Fill unknown grades as -1
